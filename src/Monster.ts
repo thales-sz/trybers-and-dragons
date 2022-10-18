0ee7ae765223f544ebed2e46d57396719a3b36a5
Thales <thales.souz@outlook.com>
2022-10-18 18:57:24 -0300

    feat: implementado class Monster, req 09

---
+++ src/Monster.ts
@@ -0,0 +1,29 @@
+import { SimpleFighter } from './Fighter';
+
+class Monster implements SimpleFighter {
+  constructor(
+    private _lifePoints: number = 85,
+    private _strength: number = 63,
+  ) {}
+
+  get strength() {
+    return this._strength;
+  }
+
+  get lifePoints() {
+    return this._lifePoints;
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const damage = attackPoints - this._lifePoints;
+    if (damage > 0) this._lifePoints -= attackPoints; 
+    if (this._lifePoints <= 0) this._lifePoints = -1;
+    return this._lifePoints;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+}
+
+export default Monster;
