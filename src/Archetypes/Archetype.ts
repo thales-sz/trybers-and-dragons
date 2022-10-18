4c8d69dc1eb546ec3434d81eff5143b4f8a1d3bf
Thales <thales.souz@outlook.com>
2022-10-18 16:11:18 -0300

    feat: Implementado getters de name, special e cost

--- src/Archetypes/Archetype.ts
+++ src/Archetypes/Archetype.ts
@@ -2,14 +2,20 @@ import { EnergyType } from '../Energy';
 
 export default abstract class Archetype {
   constructor(
-    private name: string,
-    private special: number = 0,
-    private cost: number = 0,
+    private _name: string,
+    private _special: number = 0,
+    private _cost: number = 0,
   ) {}
 
   static createdArchetypeInstances(): number {
     throw new Error('Not implemented');
   }
+
+  get name(): string { return this._name; }
+
+  get cost(): number { return this._cost; }
+
+  get special(): number { return this._special; }
 
   abstract get energyType(): EnergyType;
 }
